8991cd735c1f12e50fb856ed3d9be74f0877c9db
Asiyenur Yılmaz <asiyenurylmz17@gmail.com>
2020-11-19 22:30:41 +0300

    added user entity

--- issue-management/src/main/java/com/project/issuemanagement/entity/Issue.java
+++ issue-management/src/main/java/com/project/issuemanagement/entity/Issue.java
@@ -6,27 +6,52 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "issue")
+@Data
+@NoArgsConstructor // Boş constructor yaratıyor
+@AllArgsConstructor // Tüm field lar kullanılarak oluşturulmuş constructor
+@ToString
+@EqualsAndHashCode
 public class Issue extends BaseEntity {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	@Column(name = "description", length = 400)
 	private String description;
-	
+
+	@Column(name = "details", length = 4000)
+	private String details;
+
 	@Column(name = "date")
 	private Date date;
-	
-	@Column(name = "issueStatus")
-	@Enumerated(EnumType.STRING)  //DB de bu verinin nasıl durmasını istiyoruz?
-	private IssueStatus issueStatus;
+
+	@JoinColumn(name = "assignee_user_id")
+	@ManyToOne(optional = true, fetch = FetchType.LAZY) // Object oluşturulurken User object ataması opsiyonel
+														// //FetchType.LAZY - select atılırken Join object yalnızca
+														// getter ı çağrıldığında getirilsin
+	private User assignee;
+
+	/*
+	 * @Column(name = "issueStatus")
+	 * 
+	 * @Enumerated(EnumType.STRING) //DB de bu verinin nasıl durmasını istiyoruz?
+	 * private IssueStatus issueStatus;
+	 */
 }
